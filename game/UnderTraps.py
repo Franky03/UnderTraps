a91fca3e9d74360793ea6cb87b509c9b9e4f9207
Frankley Kaiky <kaikym20@gmail.com>
2022-04-17 22:19:12 -0300

    Level1 Create

--- game/UnderTraps.py
+++ game/UnderTraps.py
@@ -1,32 +1,42 @@
 import func
 from time import sleep
-
-print("Welcome to Under Traps.")
-print("Your mission is to find the treasure and become a conqueror.")
+death=False
+func.line(42)
+print(f"{'Welcome to Under Traps.':^42}")
+print(f"{'Your mission is to find the treasure and become a conqueror.':^42}")
+func.line(42)
 
 while True:
     try:
-        func.line(60)
+        func.line(50)
         choice1= str(input("\nYou are in a dark dungeon and there is only a bridge and a door, the bridge is old and on the other side of the door there are strange sounds, do you want to enter the 'door' or go through the 'bridge'?")).lower()
         if choice1=='door':
-            func.line(60)
+            func.line(50)
             choice2=str(input("\nYou opened the door and found a weak and sick griffin, do you want to help him? 'yes' or 'no':")).lower()
             sleep(2)
             if choice2=='yes':
-                func.line(60)
+                func.line(50)
+                print('The griffon needs to know you first, say your name and class...')
+                name= str(input('Say your name: '))
+                clas= str(input('Define your class: '))
+                txt= 'Character Profile.txt'
+                func.cadastrar(txt, name, clas)
+                already= func.arquivoExiste(txt)
+                if already == False:
+                    func.criarArquivo(txt)
                 print('You gave the healing potion that was in your backpack to the griffin, he got up and, still weak, recognized your kindness.')
                 sleep(4)
                 print("You walk around the room looking for exits... wait...")
                 sleep(4)
                 print("The griffin is walking beside you, he wants to follow you, maybe it's his survival instincts.")
                 sleep(4)
-                func.line(60)
+                func.line(50)
                 choice3= str(input("\nDo you accept having the griffin as your partner? 'yes' or 'no':")).lower()
                 if choice3== 'yes':
-                    print("Great, the griffin unexpectedly flies you out of the dungeon, maybe that was the only way out.")
+                    print("Great, the griffin unexpectedly flies you out of the dungeon, maybe that was the only way out, go to LEVEL2!")
                     break
                 else:
-                    func.line(60)
+                    func.line(50)
                     print("You decided to leave the griffin aside and didn't find any way out, go back to the beginning.")
                     sleep(4)
                     print("Now the only way is to cross the bridge, go ahead.")
@@ -34,13 +44,84 @@ while True:
                     print("Oh no, you're halfway across the bridge when the griffin runs towards you...")
                     sleep(2)
                     print("the bridge breaks!!! you fall and die... GAME OVER!")
+                    death=True
                     break
             else:
-                print("You leave the griffon aside and go back to the dungeon trying to find another way out... without looking at the ground, you step into a trap, it's very lethal... you die... GAME OVER")
+                print("You leave the griffon aside and go back to the dungeon trying to find another way out... without looking at the ground, you step into a trap, it's very lethal... before you die, say your name and class: ")
+                name= str(input('Say your name: '))
+                clas= str(input('Define your class: '))
+                txt= 'Character Profile.txt'
+                func.cadastrar(txt, name, clas)
+                already= func.arquivoExiste(txt)
+                if already == False:
+                    func.criarArquivo(txt)
                 sleep(4)
+                death=True
                 break
         else:
-            func.line(60)
-            choice2= str(input("you crossed the bridge safely, arrived outside the dungeon and found an elder almost dead, do you wish to help him? 'yes' or 'no':")).lower()
+            func.line(50)
+            choice2= str(input("You crossed the bridge safely, arrived outside the dungeon and found an elder almost dead, do you wish to help him? 'yes' or 'no':")).lower()
+            if choice2=='yes':
+                func.line(50)
+                print("You gave the healing potion that was in your backpack to the elder, the potion has a better effect on humans, healing all of the elder's wounds.")
+                quest1= str(input("The elder asked which city you were from, say the city:")).lower()
+                if quest1== 'crifina':
+                    print("Elder: Ohh, so you're from crifina, this is my hometown...thanks for the potion... what's your name?")
+                    name= str(input('Say your name: '))
+                    clas= str(input('Define your class: '))
+                    txt= 'Character Profile.txt'
+                    func.cadastrar(txt, name, clas)
+                    already= func.arquivoExiste(txt)
+                    if already == False:
+                        func.criarArquivo(txt)
+                    print(f"So, {name} of crifina, I am very grateful for your help. I will definitely see you again...bye!")
+                    print("You notice that the elder carries a staff with a jewel, but at the moment this does not pique your interest.")
+                    sleep(4)
+                    print('There in front is the gate to exit the dungeon, go to LEVEL2!')
+                    func.conq(txt, 'The elder owes you a favor')
+                    break
+                else:
+                    print(f"Elder: you're from {quest1}, i've heard many stories there, i prefer not to get involved with people of your class, thanks for the potion... what's your name?")
+                    name= str(input('Say your name: '))
+                    clas= str(input('Define your class: '))
+                    txt= 'Character Profile.txt'
+                    func.cadastrar(txt, name, clas)
+                    already= func.arquivoExiste(txt)
+                    if already == False:
+                        func.criarArquivo(txt)
+                    print(f"From now on, I'll take care of myself, {name}.")
+                    sleep(2)
+                    print('There in front is the gate to exit the dungeon, go to LEVEL2!')
+                    func.conq(txt, 'The elder hates you for your class')
+                    break
+            else:
+                func.line(50)
+                print('You left the elder for death, in fact, he is the dungeon mage and for your lack of compassion, you were sentenced to death by the mage!')
+                print('Say your name and class before die...')
+                name= str(input('Say your name: '))
+                clas= str(input('Define your class: '))
+                txt= 'Character Profile.txt'
+                func.cadastrar(txt, name, clas)
+                already= func.arquivoExiste(txt)
+                if already == False:
+                    func.criarArquivo(txt)
+                print("... GAME OVER!")
+                death= True
+                break
     except:
         print('ERROR! RELOAD THE GAME!')
+if not death:
+    while True:
+        prof= str(input("Do you want to see your profile? 'yes' or 'no': ")).lower()
+        if prof=='yes':
+            func.lerArquivo(txt)
+            break
+        elif prof=='no':
+            break
+        else:
+            print('ERROR')
+else:
+    func.lerArquivo(txt)
+
+#LEVEL 2 NO PANTANO, SOZINHO OU COM GRIFFO
+
--- game/func/__init__.py
+++ game/func/__init__.py
@@ -1,2 +1,95 @@
-def line(tam):
+def line(tam=60):
     print('--'*tam)
+def leiaInt(numero):
+    ok= False 
+    valor=0 
+    while True:
+        try:
+            n= int(input(numero))
+        except(ValueError,TypeError):
+            print('ERRO!O tipo de dado está errado.')
+        except KeyboardInterrupt:
+            print('ERRO!O usuário não informou os dados.')
+            return 0
+        else:
+            valor=n
+            ok=True
+        if ok:
+            break 
+    return valor
+
+
+def hea(txt, tam=42):
+    print(line(tam))
+    print(txt.center(42))
+    print(line(tam))
+
+def menu(lista):
+    hea('MENU PRINCIPAL')
+    c=1
+    for item in lista:
+        print(f'{c} - {item}')
+        c+=1
+    print(line())
+    opc= leiaInt('Sua opção: ')
+    return opc
+
+def arquivoExiste(nome):
+    try:
+        a= open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def criarArquivo(nome):
+    try:
+        a= open(nome, 'wt+')
+        a.close()
+    except:
+        print('File not created error!')
+    else:
+        print(f'File {nome} created successfully!')
+
+def lerArquivo(nome):
+    try:
+        a= open(nome, 'rt')
+    except:
+        print('Read file error!')
+    else:
+        hea('CHARACTER INFORMATION')
+        for linha in a:
+            dado= linha.split(';')
+            dado[1]= dado[1].replace('\n','')
+            print(f'Name:{dado[0]:<30} Class:{dado[1]:>3}')
+    finally:
+        a.close()
+
+def cadastrar(nome, n='Unknown', c= 'Human'):
+    try:
+        a= open(nome, 'at')
+    except:
+        print('File opening error!')
+    else:
+        try:
+            a.write(f'{n};{c}\n')
+        except:
+            print('error writing data!')
+        else:
+            print(f'New {n} added!')
+            a.close()
+
+def conq(nome, conquest):
+    try:
+        a= open(nome, 'at')
+    except:
+        print('File opening error!')
+    else:
+        try:
+            a.write(f'Conquest: {conquest}')
+        except:
+            print('error writing data!')
+        else:
+            print('New conquest added!')
+            a.close()
